cac8d8c0cc792e2374e3d9780dae1777d8e0beda
atul24112001 <atulmorchhlay204@gmail.com>
2024-05-01 11:53:02 +0530

    Updated workflow

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 export PATH=$PATH:/root/.nvm/versions/node/v20.9.0/bin
 
-POSTGRESS_DATABASE_PASSWORD="$1"
+cd ~/survey
+pm2 delete survey
 
-DATABASE_URL="postgres://postgres:$POSTGRESS_DATABASE_PASSWORD@164.68.103.23:1001/survey?schema=public"
-ADMIN_EMAIL="atulmorchhlay204@gmail.com"
-ADMIN_PASSWORD=$POSTGRESS_DATABASE_PASSWORD
+npm install
+npx prisma migrate deploy
+npx prisma generate
+npm run build
+pm2 start npm --name "survey" -- start
 
 
+# POSTGRESS_DATABASE_PASSWORD="$1"
+
+# # DATABASE_URL="postgres://postgres:$POSTGRESS_DATABASE_PASSWORD@164.68.103.23:1001/survey?schema=public"
+# # ADMIN_EMAIL="atulmorchhlay204@gmail.com"
+# # ADMIN_PASSWORD=$POSTGRESS_DATABASE_PASSWORD
 
 
-docker stop survey
-docker rm 
-docker rmi atul24112001/survey
+# docker stop survey
+# docker rm 
+# docker rmi atul24112001/survey
 
-cd ~/chess/apps/next-client
-docker build -t atul24112001/survey:latest .
-docker run -e ADMIN_EMAIL=$ADMIN_EMAIL \
-    -e ADMIN_PASSWORD=$ADMIN_PASSWORD \
-    -e DATABASE_URL=$DATABASE_URL \
-    --name survey \
-    --network backend_bridge \
-    -d -p 3004:3000 atul24112001/survey:latest
+# cd ~/survey
+# docker build -t atul24112001/survey:latest .
+# docker run -e ADMIN_EMAIL=$ADMIN_EMAIL \
+#     -e ADMIN_PASSWORD=$ADMIN_PASSWORD \
+#     -e DATABASE_URL=$DATABASE_URL \
+#     --name survey \
+#     --network backend_bridge \
+#     -d -p 3004:3000 atul24112001/survey:latest
